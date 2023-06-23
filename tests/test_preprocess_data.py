c81b8f7f734b4478f1091f69251b6a909b811dc0
mohamedfattouhy <78499945+mohamedfattouhy@users.noreply.github.com>
2023-06-24 00:10:20 +0200

    add some unit tests

---
+++ tests/test_preprocess_data.py
@@ -0,0 +1,46 @@
+#  MANAGEMENT ENVIRONMENT --------------------------------
+import os
+import unittest
+import tensorflow as tf
+from facematcher.data_collection.preprocessing import preprocess, preprocess_twin
+from facematcher.data_collection.preprocessing import create_dataset
+
+
+# TESTS  -------------------------------
+class PreprocessImages(unittest.TestCase):
+
+    def setUp(self):
+        self.img_path = os.path.join('tests', 'data_test', 'negative',
+                                     'Aaron_Peirsol_0001.jpg')
+
+        self.ANC_PATH = os.path.join('tests', 'data_test', 'anchor')
+        self.POS_PATH = os.path.join('tests', 'data_test', 'positive')
+        self.NEG_PATH = os.path.join('tests', 'data_test', 'negative')
+
+        self.anchor_dataset = tf.data.Dataset.list_files(self.ANC_PATH+'\*.jpg')
+        self.positive_dataset = tf.data.Dataset.list_files(self.POS_PATH+'\*.jpg')
+        self.negative_dataset = tf.data.Dataset.list_files(self.NEG_PATH+'\*.jpg')
+
+        self.data = create_dataset(self.anchor_dataset,
+                                   self.positive_dataset,
+                                   self.negative_dataset)
+
+    def test_preprocess(self):
+
+        img = preprocess(file_path=self.img_path)
+        self.assertEqual(img.shape, (100, 100, 3))
+        self.assertIsInstance(img, tf.Tensor)
+        self.assertTrue((img.numpy().max() >= 0) and (img.numpy().max() <= 1))
+
+        sample_1 = self.data.as_numpy_iterator().next()
+        self.assertIsInstance(sample_1, tuple)
+        self.assertEqual(len(sample_1), 3)
+
+        self.data_preprocessed_twin = self.data.map(preprocess_twin)
+        samples_2 = self.data_preprocessed_twin.as_numpy_iterator().next()
+        self.assertIsInstance(samples_2, tuple)
+        self.assertEqual(len(samples_2), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_siamese_model.py
@@ -0,0 +1,50 @@
+#  MANAGEMENT ENVIRONMENT --------------------------------
+import os
+import unittest
+import tensorflow as tf
+from keras.models import Model
+from facematcher.data_collection.preprocessing import create_dataset, preprocess_twin
+from facematcher.siamese_neural_network.build.build_model import make_embedding, siamese_model
+from facematcher.siamese_neural_network.train.train_model import train
+
+
+# TESTS  -------------------------------
+class TestSiamseseModel(unittest.TestCase):
+
+    def setUp(self):
+
+        self.ANC_PATH = os.path.join('tests', 'data_test', 'anchor')
+        self.POS_PATH = os.path.join('tests', 'data_test', 'positive')
+        self.NEG_PATH = os.path.join('tests', 'data_test', 'negative')
+
+        self.anchor_dataset = tf.data.Dataset.list_files(self.ANC_PATH+'\*.jpg')
+        self.positive_dataset = tf.data.Dataset.list_files(self.POS_PATH+'\*.jpg')
+        self.negative_dataset = tf.data.Dataset.list_files(self.NEG_PATH+'\*.jpg')
+
+        self.data = create_dataset(self.anchor_dataset,
+                                   self.positive_dataset,
+                                   self.negative_dataset)
+
+        self.data = self.data.map(preprocess_twin)
+        self.data = self.data.cache()
+        self.data = self.data.shuffle(buffer_size=10)
+        self.data = self.data.batch(1)
+        self.data = self.data.prefetch(1)
+
+    def test_model_type(self):
+
+        embedding = make_embedding()
+        model = siamese_model()
+
+        self.assertIsInstance(embedding, Model)
+        self.assertIsInstance(model, Model)
+
+    def test_train_model(self):
+        try:
+            train(data=self.data, epochs=1)
+        except Exception as e:
+            self.fail(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    unittest.main()
